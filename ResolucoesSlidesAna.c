2d2490bae4dc23cefda6e609277ec0c399eb8fd8
Anésio Sousa <anesios98@gmail.com>
2022-06-30 22:32:40 -0300

    Resoluções das atividades dos slides 3

--- ResolucoesSlidesAna.c
+++ ResolucoesSlidesAna.c
@@ -1,7 +1,62 @@
 #include<stdio.h>
 
 int main(){
-    printf("%d", 35);
+
+}
+
+/* Exercícios slides 3
+
+Questão 1:
+
+    float peso, altura;
+    scanf("%f %f", &peso, &altura);
+    printf("%.2f", peso/(altura*altura));
+
+Questão 2:
+
+float km;
+
+    printf("Insira a distancia percorrida:\n");
+    scanf("%f", &km);
+
+    printf("Valor cobrado pela agencia A: %.2f\n",62+1.40*km);
+    printf("Valor cobrado pela agencia B: %.2f\n",80+1.20*km);
+
+
+Questão 3:
+
+    float desconto, valorMercadoria, descontoNormalizado;
+
+    printf("Insira o valor do produto:\n");
+    scanf("%f", &valorMercadoria);
+
+    printf("Insira a porcentagem de desconto:\n");
+    scanf("%f", &desconto);
+
+    descontoNormalizado = desconto/100;
+
+    printf("Valor do desconto: %.2f\n", valorMercadoria*descontoNormalizado);
+    printf("Valor final do produto: %.2f\n",valorMercadoria*(1-descontoNormalizado));
+
+QUESTÃO 4:
+    float kg,vezes;
+
+    printf("Insira o peso da crianca e tecle enter:\n");
+    scanf("%f", &kg);
+
+    vezes = kg/2;
+    printf("Quantidade de gotas: %.f\n", vezes*5);
+
+Questão 5:
+
+    int idadeEmDias;
+
+    printf("Voce tem quantos dias de idade?\n");
+    scanf("%d", &idadeEmDias);
+
+    printf("Voce ja viveu %d anos, %d meses e %d dias\n", idadeEmDias/365, (idadeEmDias%365)/30, (idadeEmDias%365)%30);
 
     return 0;
-}
+
+
+*/
